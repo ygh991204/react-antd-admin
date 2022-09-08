4043918f2b4f62c4deba481412526255acdef3c0
Xiao Feng <2200429565@qq.com>
2022-09-08 15:24:58 +0800

    构建预览

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ygh991204/react-antd-admin.git master:gh-pages
+git push -f git@github.com:ygh991204/react-antd-admin.git main:gh-pages
 
 cd -
